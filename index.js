8dae6ece62b06a4df16404df73b84d518ef2d053
Circuit-Aurora <69444511+Circuit-Aurora@users.noreply.github.com>
2021-02-16 17:26:41 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
-const Application = require('@waline/vercel');
+const Waline = require('@waline/vercel');
 
-module.exports = Application({
-  async postSave(comment) {
-    //do what ever you want after save comment
+module.exports = Waline({
+  secureDomains: 'waline.js.org'
+
+  forbiddenWords: [
+    '傻逼',
+    '智障',
+    '没妈',
+    '草泥马',
+    '操你妈'
+  ]
+
+  async preSave(comment) {
+    const isSapm = await Akismet.check(comment);
+    if(isSpam) {
+      return { errmsg: 'It\'s a spam!' };
+    }
+  }
+
+  async postSave(comment, pComment) {
+    await mailto({
+      mail: pComment.mail,
+      text: `${comment.nick} replied your comment!`
+    });
+  }
+
+  async preUpdate(comment) {
+    return 'Then you can\'t update comment data';
+
+  async postUpdate(comment) {
+    console.log(`comment ${comment.objectId} has been updated!`);
+  }
+
+  async preDelete(commentId) {
+    return 'Then you can\'t delete comment';
+  }
+
+  async postDelete(commentId) {
+    console.log(`comment ${commentId} has been deleted!`);
   }
 });
