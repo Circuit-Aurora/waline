fb8dfe0d44a766a94c79c6c9b7e57d98a832696a
Circuit-Aurora <69444511+Circuit-Aurora@users.noreply.github.com>
2021-02-16 23:50:47 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ const Waline = require('@waline/vercel');
 
 module.exports = Waline({
   secureDomains: 'circuit-aurora.github.io'
-});
-
-module.exports = Waline({
+  
   forbiddenWords: [
     '傻',
     '智障',
@@ -15,9 +13,7 @@ module.exports = Waline({
     '习近平',
     '毛泽东'
   ]
-});
 
-module.exports = Waline({
   disallowIPList: [
     '177.18.2.206',
     '200.186.145.218',
@@ -30,45 +26,33 @@ module.exports = Waline({
     '81.94.193.29',
     '199.193.71.80'
   ]
-});
 
-module.exports = Waline({
   async preSave(comment) {
     const isSapm = await Akismet.check(comment);
     if(isSpam) {
       return { errmsg: 'It\'s a spam!' };
     }
   }
-});
 
-module.exports = Waline({
   async postSave(comment, pComment) {
     await mailto({
       mail: pComment.mail,
       text: `${comment.nick} replied your comment!`
     });
   }
-});
 
-module.exports = Waline({
   async preUpdate(comment) {
     return 'Then you can\'t update comment data';
   }
-});
 
-module.exports = Waline({
   async postUpdate(comment) {
     console.log(`comment ${comment.objectId} has been updated!`);
   }
-});
 
-module.exports = Waline({
   async preDelete(commentId) {
     return 'Then you can\'t delete comment';
   }
-});
 
-module.exports = Waline({
   async postDelete(commentId) {
     console.log(`comment ${commentId} has been deleted!`);
   }
