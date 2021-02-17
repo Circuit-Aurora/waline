9de46e58f933a73d5775f6b8db983f6bb9b3dcdd
Circuit-Aurora <69444511+Circuit-Aurora@users.noreply.github.com>
2021-02-17 15:57:28 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,16 @@ const Waline = require('@waline/vercel');
 
 module.exports = Waline({
   secureDomains: 'circuit-aurora.github.io'
+  forbiddenWords: [
+    '傻',
+    '智障',
+    '没妈',
+    '死妈',
+    '台独',
+    '港独',
+    '澳独',
+    '疆独',
+    '习近平',
+    '毛泽东'
+  ]
 });
